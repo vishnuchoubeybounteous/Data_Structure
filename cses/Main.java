7bf6c6f2ed18fd262a593a3a85d5d8de52d736bb
Vishnu Choubey <vishnu.choubey@bounteous.com>
2026-02-13 14:28:01 +0530

    Weird Algorithm

---
+++ cses/Main.java
@@ -0,0 +1,21 @@
+package cses;
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        long n=sc.nextInt();
+        
+        while(n>1){
+            System.out.print(n+" ");
+            if(n%2==0){
+                n/=2;
+            }
+            else{
+                n=n*3+1;
+            }
+        }
+        System.out.println(n);
+       
+    }
+}
