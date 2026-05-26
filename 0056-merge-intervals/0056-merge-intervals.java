29ee653e5de4c89a65e10c00dc5d94d13ce96377
vishnuchoubeybounteous <vishnu.choubey@bounteous.com>
2026-05-26 10:38:36 +0530

    Time: 9 ms (35.09%), Space: 49.1 MB (38.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> list=new ArrayList<>();
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        int min=intervals[0][0];
-        int max=intervals[0][1];
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        List<int[]> list=new ArrayList<>();
         for(int i=1;i<intervals.length;i++){
-            if(max<intervals[i][0]){
-                list.add(new int[]{min,max});
-                min=intervals[i][0];
-                max=intervals[i][1];
+            if(end<intervals[i][0]){
+                list.add(new int[]{start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
             }
-         
             else{
-                min=Math.min(min,intervals[i][0]);
-                max=Math.max(max,intervals[i][1]);
+                start=Math.min(start,intervals[i][0]);
+                end=Math.max(end,intervals[i][1]);
             }
         }
-        list.add(new int[]{min,max});
-        return list.toArray(new int[list.size()][]);
+        list.add(new int[]{start,end});
+        int[][] ans=new int[list.size()][2];
+        for(int i=0;i<list.size();i++){
+            ans[i]=list.get(i);
+        }
+        return ans;
     }
 }
