79b515d6307f601accbfa44a0d291e0cf042373e
vishnuchoubeybounteous <vishnu.choubey@bounteous.com>
2026-05-27 11:12:13 +0530

    Time: 1 ms (89.42%), Space: 43 MB (89.05%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+      if(word.length()==1)return true;
+      char ch=word.charAt(0);
+      if(Character.isLowerCase(ch)){
+         for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i)))return false;
+         }
+      }
+      else{
+          char c1=word.charAt(1);
+          if(Character.isUpperCase(c1)){
+            for(int i=1;i<word.length();i++){
+                if(Character.isLowerCase(word.charAt(i)))return false;
+            }
+          }
+          else{
+            for(int i=1;i<word.length();i++){
+                if(Character.isUpperCase(word.charAt(i)))return false;
+            }
+          }
+      }
+      return true;
+    }
+}
