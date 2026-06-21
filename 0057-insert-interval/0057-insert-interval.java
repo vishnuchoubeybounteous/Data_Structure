1e9afdca96c24ba27a0d09b08ec841e9de74c45a
vishnuchoubeybounteous <vishnu.choubey@bounteous.com>
2026-06-21 12:51:09 +0530

    Time: 0 ms (100%), Space: 46.8 MB (94.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        List<int[]> list=new ArrayList<>();
-        int start=newInterval[0];
-        int end=newInterval[1];
-        int i=0;
-        while(i<intervals.length&&intervals[i][1]<start){
-            list.add(intervals[i]);
-            i++;
-        }
-        while(i<intervals.length&&end>=intervals[i][0]){
-            start=Math.min(start,intervals[i][0]);
-            end=Math.max(end,intervals[i][1]);
-            i++;
-        }
-        list.add(new int[]{start,end});
-        while(i<intervals.length){
-            list.add(intervals[i]);
-            i++;
-        }
-        int[][] ans=new int[list.size()][2];
-       for(int j=0;j<list.size();j++){
-        ans[j]=list.get(j);
+       List<int[]> list=new ArrayList<>();
+       int start=newInterval[0];
+       int end=newInterval[1];
+       int i=0;
+       int n=intervals.length;
+       while(i<n&&intervals[i][1]<start){
+          list.add(intervals[i]);
+          i++;
+       } 
+       while(i<n&&end>=intervals[i][0]){
+          start=Math.min(start,intervals[i][0]);
+          end=Math.max(end,intervals[i][1]);
+          i++;
        }
-       return ans;
+       list.add(new int[]{start,end});
+       while(i<n){
+        list.add(intervals[i]);
+        i++;
+       }
+       int[][] arr=new int[list.size()][2];
+       for(int j=0;j<list.size();j++){
+         arr[j]=list.get(j);
+       }
+       return arr;
     }
 }
